ebdfc00a971038ea1d5972b1f1f30f8098b558e7
Ana Beatriz Oliveira <108485699+anabeatrizoliveira@users.noreply.github.com>
2022-07-31 22:49:49 -0300

    Update Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -8,7 +8,7 @@
 
 //A busca sequencial foi realizada a partir da matrícula dos alunos conseguindo por ela achar todos os dados.
 
-typedef struct {
+typedef struct aluno{
     int matricula;
     char nome[15];
     float nota01;
@@ -21,6 +21,7 @@ float media (float n1, float n2, float n3){
     float resultado;
     resultado=(n1+n2+n3)/3;
     return (resultado);
+    //Função para calcular a média
     }
 
 int buscamatricula(int matricula, cadastroaluno aluno[], int tamanhoarray){
@@ -29,8 +30,9 @@ int buscamatricula(int matricula, cadastroaluno aluno[], int tamanhoarray){
 			return i;
 		}
 	}
-	
 	return -1;
+	//Função de busca sequencial da matrícula;
+	//Retorna -1 caso a condição não seja verdadeira, logo, um número de matrícula inexistente no arquivo;
 }
 
 void bubblesort(cadastroaluno alunos[], int tamanhoarray){
@@ -43,42 +45,65 @@ void bubblesort(cadastroaluno alunos[], int tamanhoarray){
 			}
 		}
 	}
+	//Função ordenadora em ordem crescente;
+}
+
+int qtdaluno(FILE *arq){
+    int numerodelinhas = 0;
+    char c;
+    arq = fopen("alunos.txt", "rt");
+	while ((c = fgetc(arq)) != EOF){
+        if(c == '\n')
+            numerodelinhas++;
+    }
+    fclose(arq);
+    return (numerodelinhas);
+    //Função para contar a quantidade de linhas;
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	int opcao=1;
 	FILE *arq;
-	int quantidadealuno=0;
-	cadastroaluno *alunos = (cadastroaluno*) malloc (1* sizeof(cadastroaluno));
+    int contador = 0;
+    int quantidadealuno = (qtdaluno(arq)+1);
+	cadastroaluno *alunos = (cadastroaluno*) malloc (quantidadealuno* sizeof(cadastroaluno));
 	arq = fopen ("alunos.txt", "rt");
-		
-	while (!feof(arq)){
-		
-		if(quantidadealuno!=0){
-		
-		alunos = (cadastroaluno*) realloc (alunos, (sizeof(alunos)+1)*sizeof(cadastroaluno));
-	}
-		fscanf(arq, "%d %s %f %f %f", &alunos[quantidadealuno].matricula, &alunos[quantidadealuno].nome, &alunos[quantidadealuno].nota01, &alunos[quantidadealuno].nota02, &alunos[quantidadealuno].nota03);
-		alunos[quantidadealuno].media=media(alunos[quantidadealuno].nota01, alunos[quantidadealuno].nota02, alunos[quantidadealuno].nota03);
-		
-		printf("%d %s %.2f %.2f %.2f %.2f \n", alunos[quantidadealuno].matricula, alunos[quantidadealuno].nome, alunos[quantidadealuno].nota01, alunos[quantidadealuno].nota02, alunos[quantidadealuno].nota03, alunos[quantidadealuno].media);
-		quantidadealuno++;	
+
+  
+    while (contador < quantidadealuno){
+        char nota01temp[4];
+        char nota02temp[4];
+        char nota03temp[4];
+		//como o programa está em português, estas linhas são necessárias para não dar bug com número float com ponto ao invés de vírgula;
+		fscanf(arq, "%d %s %s %s %s", &alunos[contador].matricula, alunos[contador].nome, nota01temp, nota02temp, nota03temp);
+        alunos[contador].nota01 = atof(nota01temp);
+        alunos[contador].nota02 = atof(nota02temp);
+        alunos[contador].nota03 = atof(nota03temp);
+        alunos[contador].media=media(alunos[contador].nota01, alunos[contador].nota02, alunos[contador].nota03);		
+		// o próximo printf era apenas para teste;
+		//printf("\n%d %s %.2f %.2f %.2f %.2f \n", alunos[contador].matricula, alunos[contador].nome, alunos[contador].nota01, alunos[contador].nota02, alunos[contador].nota03, alunos[contador].media);
+        contador++;
 	}
 	
-	
+    fclose(arq);
+
+
+	int opcao=1;
 	while(opcao!=0){
-		
 	
-	
-	
-	printf("\n\nDigite o código que faça a ação que deseja executar:\n1- Imprimir todos os Elementos do Arranjo. \n2- Imprimir apenas os nomes dos alunos. \n3- Imprimir os nomes dos alunos e suas respectivas notas finais. \n4- Buscar os dados de um aluno usando a busca sequencial. \n5- Imprimir os alunos ordenados de acordo com sua matrícula. \n6- Editar as notas de um aluno, para isso é necessário que se busque os dados do aluno. \n7- Imprimir a matrícula, o nome, e a nota final do aluno que obteve a maior nota de todas. \n8- Imprimir a matrícula, nome e nota final do aluno que obteve a menor nota de todas. \n9-Imprimir quantos alunos foram reprovados, imprima ainda a média geral. Os alunos aprovados serão aqueles que obtiverem média maior ou igual a 6.0. \n0- Sair do programa\n");
+	printf("Digite o código que faça a ação que deseja executar:\n1- Imprimir todos os Elementos do Arranjo. \n2- Imprimir apenas os nomes dos alunos. \n3- Imprimir os nomes dos alunos e suas respectivas notas finais. \n4- Buscar os dados de um aluno usando a busca sequencial. \n5- Imprimir os alunos ordenados de acordo com sua matrícula. \n6- Editar as notas de um aluno, para isso é necessário que se busque os dados do aluno. \n7- Imprimir a matrícula, o nome, e a nota final do aluno que obteve a maior nota de todas. \n8- Imprimir a matrícula, nome e nota final do aluno que obteve a menor nota de todas. \n9-Imprimir quantos alunos foram reprovados, imprima ainda a média geral. Os alunos aprovados serão aqueles que obtiverem média maior ou igual a 6.0. \n0- Sair do programa\n");
 	scanf("%i", &opcao);
 	
 		if(opcao==0){
+		FILE *arqnovo = fopen ("alunosorganizados.txt", "wt");
+		bubblesort(alunos, quantidadealuno);
+			for(int j=0; j<quantidadealuno; j++){
+            fprintf(arqnovo, "%d %s %.2f %.2f %.2f %.2f %d \n", alunos[j].matricula, alunos[j].nome, alunos[j].nota01, alunos[j].nota02, alunos[j].nota03, alunos[j].media);
+			}
 	    exit(0);
 	    return(0);
+	    //A função para salvar um novo arquivo está para quando o usuário digitar 0, porque além de salvar ordenado o arquivo, ela salva as alterações feitas quando o menu 6 for selecionado;
 		}
 		
 		else if(opcao==1){
@@ -116,8 +141,8 @@ int main()
 		
 		else if(opcao==5){
 		bubblesort(alunos, quantidadealuno);
-			for(int i=0; i<quantidadealuno; i++){
-			printf("%d %s %.2f %.2f %.2f %.2f %d \n", alunos[i].matricula, alunos[i].nome, alunos[i].nota01, alunos[i].nota02, alunos[i].nota03, alunos[i].media);
+			for(int j=0; j<quantidadealuno; j++){
+			printf("Matrícula: %i. Nome: %s. Nota01: %.2f. Nota02: %.2f. Nota03: %.2f. Média: %.2f. \n\n", alunos[j].matricula, alunos[j].nome, alunos[j].nota01, alunos[j].nota02, alunos[j].nota03, alunos[j].media);
 			}
 	}
 	
@@ -131,6 +156,7 @@ int main()
 					printf("Digite um número de matrícula válido.");
 				}
 				else{
+					//O novo menu é para o usuário selecionar qual dado ele deseja editar, porém, criei sem repetição, caso o usuário deseje editar outro dado do aluno, deve selecionar a opção no menu principal para isso e repetir o processo;
 					printf("Digite o códio que corresponde ao dado que você deseja editar:\n1- Número de matrícula.\n2- Nome do aluno.\n3- Nota 1.\n4- Nota 2.\n5- Nota 3.\n\n");
 					scanf("%i", &dado);
 						if(dado==1){
@@ -196,10 +222,12 @@ int main()
 			mediageral=mediageral+alunos[j].media;
 			}
 			mediageral=mediageral/quantidadealuno;
-			printf("Foram aprovador %i alunos.\nForam reprovados %i alunos. \nA média geral de todas as notas é: %.2f.\n\n", contaprovado, contreprovado, mediageral);	
+			printf("Foram aprovador %i alunos.\nForam reprovados %i alunos. \nA média geral de todas as notas é: %.2f.\n\n", contaprovado, contreprovado, mediageral);		
     		}
 	
 	}
+	
+	//Limpando a memória alocada dinâmicamente. 
  	free(alunos);
 	return 0;
 	}
